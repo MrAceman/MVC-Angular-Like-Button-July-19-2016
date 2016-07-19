10a05e94a53057ec1bda1cb5dd52f795721c6e8d
Niclas Willems <niclas.willems@gmail.com>
2016-07-19 13:25:41 -0400

    Improved conditional logic for if

--- src/js/controllers/main.controller.js
+++ src/js/controllers/main.controller.js
@@ -3,10 +3,7 @@ function MainController ($scope){
   $scope.numOfLikes = '';
 
     $scope.totalLikes = function (numOfLikes){
-      if (numOfLikes === 0){
-        return (numOfLikes + " likes")
-      }
-      else if (numOfLikes === 1){
+      if (numOfLikes === 1){
         return (numOfLikes + " like")
       }
       else {
